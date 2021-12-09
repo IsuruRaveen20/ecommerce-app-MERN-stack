20be9b49d60d3c71a9fb823c252514a0659d5b7c
isururaveen <it19152592@my.sliit.lk>
2021-12-09 22:51:07 +0530

    minor updates to server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require ('cors');
+const morgan = require('morgan');
 const connectDB = require('./database/db');
+
+//Middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
 connectDB();
 
 const port = process.env.PORT || 5000;
